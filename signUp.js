5603a9d49c6ce68245f342de2de869cacafcfec7
Michael-Emad-Ramzy <michaelebeid9@gmail.com>
2024-07-09 14:35:18 +0300

    edit boxes and adding the rest logic

--- signUp.js
+++ signUp.js
@@ -1,32 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const fullNameInput = document.getElementById("fullName");
+  const firstNameInput = document.getElementById("firstName");
+  const lastNameInput = document.getElementById("lastName");
   const emailInput = document.getElementById("email");
   const passwordInput = document.getElementById("password");
   const repeatPasswordInput = document.getElementById("repeatPassword");
-  const nameError = document.getElementById("nameError");
+  const firstNameError = document.getElementById("firstNameError");
+  const lastNameError = document.getElementById("lastNameError");
   const emailError = document.getElementById("emailError");
   const passwordError = document.getElementById("passwordError");
   const repeatPasswordError = document.getElementById("repeatPasswordError");
   const genderInput = document.getElementById("gender");
   const genderError = document.getElementById("genderError");
+  const birthdayInput = document.getElementById("birthday");
+  const birthdayError = document.getElementById("birthdayError");
+  const cityInput = document.getElementById("city");
+  const cityError = document.getElementById("cityError");
   const form = document.getElementById("registerButton");
-  // fullNameInput.addEventListener('focus', () => {
-  //     fullNameInput.style.border = 'solid 0.25px red';
+  // firstNameInput.addEventListener('focus', () => {
+  //     firstNameInput.style.border = 'solid 0.25px red';
   // });
-  fullNameInput.addEventListener("blur", () => {
-    fullNameInput.style.border = "";
-    if (fullNameInput.value.length < 3) {
-      nameError.textContent = "Invalid name";
-      nameError.style.display = "block";
-      nameError.style.marginTop = "0px";
-      fullNameInput.select();
-      fullNameInput.style.borderBottom = "solid 0.2px red";
+  firstNameInput.addEventListener("blur", () => {
+    firstNameInput.style.border = "";
+    if (firstNameInput.value.length < 3) {
+      firstNameError.textContent = "Invalid name";
+      firstNameError.style.display = "block";
+      firstNameError.style.marginTop = "0px";
+      firstNameInput.select();
+      firstNameInput.style.borderBottom = "solid 0.3px red";
     } else {
-      nameError.textContent = "";
-      nameError.style.display = "none";
-      fullNameInput.style.borderBottom = "solid 0.2px green";
+      firstNameError.textContent = "";
+      firstNameError.style.display = "none";
+      firstNameInput.style.borderBottom = "solid 0.3px green";
     }
   });
+
+  lastNameInput.addEventListener("blur", () => {
+    lastNameInput.style.border = "";
+    if (lastNameInput.value.length < 3) {
+      lastNameError.textContent = "Invalid name";
+      lastNameError.style.display = "block";
+      lastNameError.style.marginTop = "0px";
+      lastNameInput.select();
+      lastNameInput.style.borderBottom = "solid 0.3px red";
+    } else {
+      lastNameError.textContent = "";
+      lastNameError.style.display = "none";
+      lastNameInput.style.borderBottom = "solid 0.3px green";
+    }
+  });
+
   emailInput.addEventListener("blur", () => {
     emailInput.style.borderBottom = "";
     const emailValue = emailInput.value;
@@ -35,24 +57,56 @@ document.addEventListener("DOMContentLoaded", () => {
       emailError.textContent = "Invalid email";
       emailError.style.display = "block";
       emailError.style.color = "red";
-      emailInput.style.borderBottom = "solid 0.2px red";
+      emailInput.style.borderBottom = "solid 0.3px red";
       emailInput.focus();
       emailInput.select();
     } else {
       emailError.textContent = "";
-      emailInput.style.borderBottom = "solid 0.2px green";
+      emailInput.style.borderBottom = "solid 0.3px green";
     }
   });
+
   genderInput.addEventListener("blur", () => {
     if (genderInput.value === "") {
       genderError.textContent = "Please select a gender.";
       genderError.style.display = "block";
-      genderInput.style.borderBottom = "solid 0.2px red";
+      genderInput.style.borderBottom = "solid 0.3px red";
       isValid = false;
     } else {
       genderError.textContent = "";
       genderError.style.display = "none";
-      genderInput.style.borderBottom = "solid 0.2px green";
+      genderInput.style.borderBottom = "solid 0.3px green";
+    }
+  });
+
+  birthdayInput.addEventListener("blur", () => {
+    const birthdayValue = new Date(birthdayInput.value);
+    const today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+
+    if (birthdayValue >= today || isNaN(birthdayValue.getTime())) {
+      birthdayError.textContent =
+        "Please enter a valid date of birth in the past.";
+      birthdayError.style.display = "block";
+      birthdayInput.style.borderBottom = "solid 0.3px red";
+    } else {
+      birthdayError.textContent = "";
+      birthdayError.style.display = "none";
+      birthdayInput.style.borderBottom = "solid 0.3px green";
+    }
+  });
+
+  cityInput.addEventListener("blur", () => {
+    if (cityInput.value === "") {
+      cityError.textContent = "Please select a gender.";
+      cityError.style.display = "block";
+      cityInput.style.borderBottom = "solid 0.3px red";
+      isValid = false;
+    } else {
+      cityError.textContent = "";
+      cityError.style.display = "none";
+      cityInput.style.borderBottom = "solid 0.3px green";
     }
   });
 
@@ -78,13 +132,13 @@ document.addEventListener("DOMContentLoaded", () => {
         "Password should contain both numbers and letters";
       passwordError.style.display = "block";
       passwordError.style.color = "red";
-      passwordInput.style.borderBottom = "solid 0.2px red";
+      passwordInput.style.borderBottom = "solid 0.3px red";
       passwordInput.focus();
       passwordInput.select();
     } else {
       passwordError.textContent = "";
       passwordError.style.display = "none";
-      passwordInput.style.borderBottom = "solid 0.2px green";
+      passwordInput.style.borderBottom = "solid 0.3px green";
     }
   });
 
@@ -95,21 +149,21 @@ document.addEventListener("DOMContentLoaded", () => {
       repeatPasswordError.style.display = "block";
       repeatPasswordInput.focus();
       repeatPasswordInput.style.backgroundColor = "transparent";
-      repeatPasswordInput.style.borderBottom = "solid 0.2px red";
+      repeatPasswordInput.style.borderBottom = "solid 0.3px red";
     } else {
       repeatPasswordError.style.display = "";
       repeatPasswordInput.style.backgroundColor = "transparent";
-      repeatPasswordInput.style.borderBottom = "solid 0.2px green";
-      passwordInput.style.borderBottom = "solid 0.2px green";
+      repeatPasswordInput.style.borderBottom = "solid 0.3px green";
+      passwordInput.style.borderBottom = "solid 0.3px green";
     }
   });
   form.addEventListener("submit", (event) => {
-    if (fullNameInput.value.length < 3) {
-      nameError.textContent = "Invalid name";
-      nameError.style.display = "block";
-      fullNameInput.focus();
-      fullNameInput.select();
-      fullNameInput.style.backgroundColor = "gray";
+    if (firstNameInput.value.length < 3) {
+      firstNameError.textContent = "Invalid name";
+      firstNameError.style.display = "block";
+      firstNameInput.focus();
+      firstNameInput.select();
+      firstNameInput.style.backgroundColor = "gray";
       event.preventDefault();
     } else if (passwordInput.value !== repeatPasswordInput.value) {
       passwordError.textContent =
@@ -120,9 +174,9 @@ document.addEventListener("DOMContentLoaded", () => {
       repeatPasswordInput.style.backgroundColor = "gray";
       event.preventDefault();
     } else {
-      nameError.style.display = "none";
+      firstNameError.style.display = "none";
       passwordError.style.display = "none";
-      fullNameInput.style.backgroundColor = "white";
+      firstNameInput.style.backgroundColor = "white";
       repeatPasswordInput.style.backgroundColor = "white";
     }
   });
