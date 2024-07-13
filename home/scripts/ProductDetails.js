dedaa85676dbf005b602e971484d9223dfae2b12
BasmalaSalem <basmala.mahmoud.salem@gmail.com>
2024-07-14 00:10:48 +0300

    restructure the files

--- home/scripts/ProductDetails.js
+++ home/scripts/ProductDetails.js
@@ -11,109 +11,127 @@ let productStock;
 let productImages;
 let productThumbnails;
 
+document.addEventListener("DOMContentLoaded", () => {
+  const selectedCategory = localStorage.getItem("selectedCategory");
+  if (selectedCategory) {
+    fetchData(selectedCategory);
+  } else {
+    console.error("No category selected");
+  }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
-    // function fetchData(productId) {
-    //     const singleProductApiUrl = `https://dummyjson.com/products/${productId}`;
-    // }
+  function fetchData(productId) {
+    const singleProductApiUrl = `https://dummyjson.com/products/${productId}`;
+
     fetch(singleProductApiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            productData = data;
-            productTitle = productData.title
-            productDescription = productData.description
-            productPrice = productData.price
-            productBrand = productData.brand;
-            productDiscountPercentage = productData.discountPercentage
-            productStock = productData.stock;
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        productData = data;
+        productTitle = productData.title
+        productDescription = productData.description
+        productPrice = productData.price
+        productBrand = productData.brand;
+        productDiscountPercentage = productData.discountPercentage
+        productStock = productData.stock;
 
-            productImages = productData.images;
-            productThumbnails = productData.thumbnail;
-
-
-            // Update the HTML elements with the fetched data
-            updateProductDetails();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    function updateProductDetails() {
-        document.getElementById('product-title').innerText = productTitle;
-        document.getElementById('product-brand').innerText = productBrand;
-        document.getElementById('product-description').innerText = productDescription;
-        document.getElementById('product-price').innerText = productPrice;
-        document.getElementById('discount-percentage').innerText = `${productDiscountPercentage}%`;
-        document.getElementById('product-after-discount').innerText = getPrice()
-        document.getElementById('product-stock').innerText = `Left in Stock ${productStock}`
-        document.getElementById('product-img').src = productImages;
-        document.getElementById('product-img2').src = productImages;
-        const imageUrls = [
-            productImages,
-            productImages,
-            productImages
-            // 'https://images.pexels.com/photos/355508/pexels-photo-355508.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            // 'https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg'
-        ];
-        const imageContainer = document.getElementById('main-img');
-        const imageContainer2 = document.getElementById('main-img2');
-        imageUrls.forEach((url, index) => {
-            const div = document.createElement('div');
-            const img = document.createElement('img');
-            img.src = url;
-            img.id = `product-thumbnail${index}`;
-            img.alt = 'product-img';
-            img.width = 150;
-            img.height = 150;
+        productImages = productData.images;
+        productThumbnails = productData.thumbnail;
 
 
-            img.addEventListener('click', () => {
-                const allDivs = document.querySelectorAll('#main-img > div');
-                allDivs.forEach(d => d.classList.remove('active'));
-                div.classList.add('active');
-                document.getElementById('product-img').src = url;
-                document.getElementById('product-img2').src = url;
-            });
+        // Update the HTML elements with the fetched data
+        updateProductDetails();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  function updateProductDetails() {
+    document.getElementById('product-title').innerText = productTitle;
+    document.getElementById('product-brand').innerText = productBrand;
+    document.getElementById('product-description').innerText = productDescription;
+    document.getElementById('product-price').innerText = productPrice;
+    document.getElementById('discount-percentage').innerText = `${productDiscountPercentage}%`;
+    document.getElementById('product-after-discount').innerText = getPrice()
+    document.getElementById('product-stock').innerText = `Left in Stock ${productStock}`
+    document.getElementById('product-img').src = productImages;
+    document.getElementById('product-img2').src = productImages;
+    const imageUrls = [
+      productImages,
+      productImages,
+      productImages
+      // 'https://images.pexels.com/photos/355508/pexels-photo-355508.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      // 'https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg'
+    ];
+    const imageContainer = document.getElementById('main-img');
+    const imageContainer2 = document.getElementById('main-img2');
+    imageUrls.forEach((url, index) => {
+      const div = document.createElement('div');
+      const img = document.createElement('img');
+      img.src = url;
+      img.id = `product-thumbnail${index}`;
+      img.alt = 'product-img';
+      img.width = 150;
+      img.height = 150;
 
-            div.appendChild(img);
-            imageContainer.appendChild(div);
-        });
-        imageUrls.forEach((url, index) => {
-            const div = document.createElement('div');
-            const img = document.createElement('img');
-            img.src = url;
-            img.id = 'product-thumbnail${index}';
-            img.alt = 'product-img';
-            img.width = 150;
-            img.height = 150;
+
+      img.addEventListener('click', () => {
+        const allDivs = document.querySelectorAll('#main-img > div');
+        allDivs.forEach(d => d.classList.remove('active'));
+        div.classList.add('active');
+        document.getElementById('product-img').src = url;
+        document.getElementById('product-img2').src = url;
+      });
+
+      div.appendChild(img);
+      imageContainer.appendChild(div);
+    });
+    imageUrls.forEach((url, index) => {
+      const div = document.createElement('div');
+      const img = document.createElement('img');
+      img.src = url;
+      img.id = 'product-thumbnail${index}';
+      img.alt = 'product-img';
+      img.width = 150;
+      img.height = 150;
 
 
-            img.addEventListener('click', () => {
-                const allDivs = document.querySelectorAll('#main-img > div');
-                allDivs.forEach(d => d.classList.remove('active'));
-                div.classList.add('active');
-                document.getElementById('product-img').src = url;
-                document.getElementById('product-img2').src = url;
-            });
+      img.addEventListener('click', () => {
+        const allDivs = document.querySelectorAll('#main-img > div');
+        allDivs.forEach(d => d.classList.remove('active'));
+        div.classList.add('active');
+        document.getElementById('product-img').src = url;
+        document.getElementById('product-img2').src = url;
+      });
 
-            div.appendChild(img);
-            imageContainer2.appendChild(div);
-        });
-    }
+      div.appendChild(img);
+      imageContainer2.appendChild(div);
+    });
+  }
+
+  // const selectedCategory = localStorage.getItem("selectedCategory");
+  // if (selectedCategory) {
+  //   fetchData(selectedCategory);
+  // } else {
+  //   console.error("No category selected");
+  // }
+  fetchData(1);
 
 })
 
 function getPrice() {
-    if (productDiscountPercentage != 0) {
-        var priceAfterDiscount = productPrice - (productPrice * productDiscountPercentage / 100)
-        priceAfterDiscount = priceAfterDiscount.toFixed(2)
-        return priceAfterDiscount
-    }
-    else
-        return productPrice
+  if (productDiscountPercentage != 0) {
+    var priceAfterDiscount = productPrice - (productPrice * productDiscountPercentage / 100)
+    priceAfterDiscount = priceAfterDiscount.toFixed(2)
+    return priceAfterDiscount
+  }
+  else
+    return productPrice
 }
 
 /////////////////////main//////////////////////////////////////////////////
@@ -168,7 +186,8 @@ plus.addEventListener("click", () => {
 });
 
 cartIcon.addEventListener("click", () => {
-  location.href = "cart.html";
+  // location.href = "cart.html";
+  location.href = "../html/cart.html";
 });
 
 
