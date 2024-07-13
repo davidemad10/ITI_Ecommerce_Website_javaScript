fc24477fc8161153623470e1069af902b139ad77
Abram Naguib <engabram.93@gmail.com>
2024-07-13 16:50:38 +0300

    Adding the code of Friday

--- home/category.js
+++ home/category.js
@@ -1,3 +1,31 @@
+// Fetching the Header, Nav Bar and Footer
+
+document.addEventListener('DOMContentLoaded', () => {
+    const headerPH = document.getElementById('headerPH');
+    const logoPH = document.getElementById('logoPH');
+    const navPH = document.getElementById('navPH');
+    const footerPH = document.getElementById('footerPH');
+  
+    fetch('HNF.html')
+      .then((response) => response.text())
+      .then((data) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+  
+        const header = doc.querySelector('header');
+        const logo = doc.querySelector('.logo');
+        const nav = doc.querySelector('nav');
+        const footer = doc.querySelector('footer');
+  
+        if (header) headerPH.innerHTML = header.innerHTML;
+        if (logo) logoPH.innerHTML = logo.innerHTML;
+        if (nav) navPH.innerHTML = nav.innerHTML;
+        if (footer) footerPH.innerHTML = footer.innerHTML;
+      })
+      .catch((error) => console.error('Error fetching HNF.html:', error));
+  });
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 document.addEventListener('DOMContentLoaded', () => {
     const category = localStorage.getItem('category');
     if (category) {
